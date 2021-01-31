4f2289bc0754946024ce1cf1aa7aa5ae705acc39
Steve Wan <stevelovesroblox@gmail.com>
2021-01-30 20:31:50 -0500

    peanut

--- web1/web1/urls.py
+++ web1/web1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01 import views
+
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
